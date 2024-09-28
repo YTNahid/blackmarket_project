088c58d4bcc60e3e4aeee7df74610b3e4812b880
YTNahid <nahid1coc@gmail.com>
2024-09-28 22:45:45 +0600

    updated database

--- database/bm.sql
+++ database/bm.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: sql301.infinityfree.com
--- Generation Time: Sep 14, 2024 at 08:06 AM
--- Server version: 10.6.19-MariaDB
--- PHP Version: 7.2.22
+-- Host: 127.0.0.1
+-- Generation Time: Sep 28, 2024 at 06:44 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `if0_37304778_bm`
+-- Database: `bm`
 --
 
 -- --------------------------------------------------------
@@ -42,10 +41,10 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `email`, `order_date`, `total`, `items`, `status`) VALUES
-(1, 'tester@gmail.com', '2024-09-13 16:42:11', '299.98', '[{\"name\":\"AKM\",\"price\":99.99,\"quantity\":1},{\"name\":\"M416\",\"price\":199.99,\"quantity\":1}]', 'confirmed'),
-(2, 'tester@gmail.com', '2024-09-13 18:52:55', '1038.90', '[{\"name\":\"AKM\",\"price\":99.99,\"quantity\":7},{\"name\":\"K2\",\"price\":112.99,\"quantity\":3}]', 'confirmed'),
-(3, 'tester@gmail.com', '2024-09-13 19:04:41', '112.99', '[{\"name\":\"K2\",\"price\":112.99,\"quantity\":1}]', 'canceled'),
-(4, '2002anamulhasan@gamil.com', '2024-09-14 06:06:34', '99.99', '[{\"name\":\"AKM\",\"price\":99.99,\"quantity\":1}]', 'canceled');
+(1, 'tester@gmail.com', '2024-09-13 18:59:19', 96.99, '[{\"name\":\"AKM\",\"price\":96.99,\"quantity\":1}]', 'canceled'),
+(2, 'tester@gmail.com', '2024-09-17 00:00:26', 300.00, '[{\"name\":\"test\",\"price\":100,\"quantity\":3}]', 'canceled'),
+(3, 'tester@gmail.com', '2024-09-17 00:59:21', 100.00, '[{\"name\":\"test\",\"price\":100,\"quantity\":1}]', 'confirmed'),
+(4, 'tester@gmail.com', '2024-09-17 01:27:02', 11170.00, '[{\"name\":\"hehe\",\"price\":100,\"quantity\":3},{\"name\":\"321421\",\"price\":5435,\"quantity\":2}]', 'pending');
 
 -- --------------------------------------------------------
 
@@ -58,17 +57,22 @@ CREATE TABLE `products` (
   `name` varchar(255) NOT NULL,
   `image` varchar(255) NOT NULL,
   `price` decimal(10,2) NOT NULL,
-  `added_by` varchar(255) NOT NULL
+  `added_by` varchar(50) NOT NULL,
+  `type` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `image`, `price`, `added_by`) VALUES
-(1, 'AKM', 'akm.png', '99.99', 'YTNahid'),
-(5, 'M416', 'm416.png', '199.99', 'YTNahid'),
-(6, 'K2', 'k2.png', '112.99', 'YTNahid');
+INSERT INTO `products` (`id`, `name`, `image`, `price`, `added_by`, `type`) VALUES
+(1, 'AKM', 'Akm_new.png', 96.99, '', ''),
+(2, '123', 'img-weapons-k2.png', 21.00, 'YTNahid', ''),
+(5, 'hehe', 'mk47_mutant.png', 100.00, 'YTNahid', 'ar'),
+(6, '321421', 'groza.png', 5435.00, 'YTNahid', 'ar'),
+(7, '3414122', 'qbz95.png', 423523.00, 'YTNahid', 'ar'),
+(8, 'AKM', 'akm.png', 3214.00, 'YTNahid', 'ar'),
+(9, '44', 'akm.png', 423.00, 'YTNahid', 'smg');
 
 -- --------------------------------------------------------
 
@@ -93,12 +97,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `contact`, `gender`, `birth_date`, `role`, `created_at`) VALUES
-(1, 'YTNahid', 'ytnahid@team.com', '$2y$10$xPW1FvggTe8Lvd8zoWYXE.zNKwgVLokEH.mk47cnEPVc1IkRj.36i', '01964800828', 'male', '2001-08-05', 'admin', '2024-09-13 14:54:47'),
-(2, 'Taizol', 'taizol@team.com', '$2y$10$QScVKc8fv6BdFAZchQGyT.nwddKZlMo8GHAFQGdwqf/a7WSWQYVua', '01000000000', 'male', '2000-02-02', 'admin', '2024-09-13 14:56:43'),
-(3, 'Ismayl', 'ismayl@team.com', '$2y$10$6qhUba0tjrV5UzqcHyv0S.b.kn12C49TCa8AmKKWYByfZ926n/Vca', '01000000000', 'male', '2000-02-02', 'admin', '2024-09-13 14:57:15'),
-(4, 'Tester', 'tester@gmail.com', '$2y$10$rrV6HStQ.SW7yq5uw4RYneeAsSACAcVAcnuobxwLJsmE8/6Yj7VT.', '01964800828', 'male', '2024-09-11', 'customer', '2024-09-13 14:55:53'),
-(5, 'Aurnob', 'aurgho13aurnob@gmail.com', '$2y$10$apD1ycOaDWYL8V/mct917.uEHZevsR8U6vBixoV.ytJBxQE9yW1JS', '01927881774', 'male', '2024-09-14', 'customer', '2024-09-13 18:46:29'),
-(6, 'anamul2002', '2002anamulhasan@gamil.com', '$2y$10$NAkmncVN0wDmtoMaAQ8xfeC4iOMytIQzABAcSKC5wVxq2Ni3SUBVC', '01306575021', 'male', '2002-01-01', 'customer', '2024-09-14 06:05:32');
+(1, 'YTNahid', 'ytnahid@team.com', '$2y$10$N3QPbBSgUQ8JgYZ7zvoSB.qKzLIFJULKbx0Y6J.Oh75u7egdsXBUm', '01964800828', 'male', '2001-08-05', 'admin', '2024-09-13 14:54:47'),
+(3, 'Taizol', 'taizol@team.com', '$2y$10$QScVKc8fv6BdFAZchQGyT.nwddKZlMo8GHAFQGdwqf/a7WSWQYVua', '01000000000', 'male', '2000-02-02', 'admin', '2024-09-13 14:56:43'),
+(4, 'Ismayl', 'ismayl@team.com', '$2y$10$6qhUba0tjrV5UzqcHyv0S.b.kn12C49TCa8AmKKWYByfZ926n/Vca', '01000000000', 'male', '2000-02-02', 'admin', '2024-09-13 14:57:15'),
+(16, 'tester', 'tester@gmail.com', '$2y$10$Nxt5ioW7zoHPtOLyOYs6WuHKUxznAq.X9k8nXUI/WNP2snIMwKGTm', '1234', 'male', '1111-01-01', 'customer', '2024-09-23 17:30:51');
 
 --
 -- Indexes for dumped tables
@@ -138,13 +140,13 @@ ALTER TABLE `orders`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
